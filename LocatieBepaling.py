764ca8c89e8ad6b4dadeb6cd604be1c2b6f1e783
HarmGoethals <38905900+HarmGoethals@users.noreply.github.com>
2018-05-02 09:56:25 +0200

    Add files via upload

---
+++ LocatieBepaling.py
@@ -0,0 +1,21 @@
+import scipy.io as sio
+import numpy as np
+import matplotlib
+matplotlib.use('Qt5Agg')
+import matplotlib.pyplot as plt
+
+mat_contents = sio.loadmat('Dataset1.mat')
+setH = mat_contents['H']
+
+print("eerste dimensie "+str(len(setH))+"\n")
+print("tweede dimensie "+str(len(setH[0]))+"\n")
+print("derde dimensie "+str(len(setH[0][0]))+"\n")
+
+aantal1 = len(setH)
+data1 = np.zeros(aantal1)
+
+for i in range(aantal1):
+    data1[i] = setH[i][0][0]
+
+plt.plot(data1)
+plt.show()
