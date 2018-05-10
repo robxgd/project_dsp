b827796a2ed4ab33f98b7e317fa1558cc885d705
HarmGoethals <38905900+HarmGoethals@users.noreply.github.com>
2018-05-10 16:21:44 +0200

    Werk 10/05

--- LocatieBepaling.py
+++ LocatieBepaling.py
@@ -5,6 +5,7 @@ matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 from scipy.signal import *
 from scipy.fftpack import *
+from math import *
 
 
 def channel2APDP(bestand):
@@ -32,25 +33,74 @@ def channel2APDP(bestand):
             for k in range(metingen):
                 APDP[i][j] += PDP[i][k][j]
 
+    APDP = np.divide(APDP,metingen)
+
     # for i in range(locaties):
     #     plt.plot(APDP[i])
     #     plt.show()
 
-    APDP = np.divide(APDP,metingen)
     return APDP
 
 def APDP2delays(APDP):
-    delays = np.zeros([12,2],dtype=np.double)
+    delays = np.zeros([12,2,2],dtype=np.double)
     for i in range(len(APDP)):
-        tijden = argrelmin(APDP[i])
-        for tijd in range(len(tijden)):
-            waarden = APDP[i][tijden[tijd]]
-        print(waarden)
+        max = np.amax(APDP[i])
+        tijden = argrelextrema(APDP[i], np.greater)
+        maxima = APDP[i][tijden]
+        maxima_sort = np.copy(maxima)
+        maxima_sort = np.sort(maxima_sort)
+        maxima_sort = maxima_sort[::-1]
+
+        delays[i][0][0] = np.nonzero(APDP[i] == maxima_sort[0])[0]
+        delays[i][0][1] = maxima_sort[0]
+        index = 1
+        while(abs(np.nonzero(APDP[i] == maxima_sort[index])[0]-np.nonzero(APDP[i] == maxima_sort[0])[0]) < 5):
+            index = index + 1
+
+
+        delays[i][1][0] = np.nonzero(APDP[i] == maxima_sort[index])[0]
+        delays[i][1][1] = maxima_sort[index]
     return delays
 
+def calculate_location(delays):
+    c = 3*10**8
+    plaatsen = np.zeros([12,2], dtype=np.double)
+    locaties = len(delays)
+    for i in range(locaties):
+        freq1 = (10**9 + delays[i][0][0]*10**7)
+        freq2 = (10**9 + delays[i][1][0]*10**7)
+        periode1 = 1/freq1
+        periode2 = 1/freq2
+
+        t_0 = periode1*delays[i][0][0]
+        t_1 = periode2*delays[i][1][0]
+
+        plaatsen[i][0] = sqrt((c*t_0)**2 - (((((c*t_1)**2-(c*t_0)**2)/4)- 1)**2))
+        plaatsen[i][1] = ((c*t_1)**2 - (c*t_0)**2)/4
+
+    print plaatsen
+    return plaatsen
+
+def precies_lokatie():
+    plaatsen = np.zeros([12,2], dtype=np.double)
+    for i in range(12):
+        plaatsen[i][0] = 8+cos(i*pi/6)
+        plaatsen[i][1] = 8+sin(i*pi/6)
+
+    print(plaatsen)
+    return plaatsen
 def main():
     APDP = channel2APDP("Dataset1.mat")
-    APDP2delays(APDP)
+    plaatsen  = APDP2delays(APDP)
+    calc_locs = calculate_location(plaatsen)
+    prec_locs = precies_lokatie()
+    plt.figure()
+    for i in range (12):
+        plt.plot(prec_locs[i][0], prec_locs[i][1], 'ro')
+        plt.plot(calc_locs[i][0], calc_locs[i][1], 'go')
+
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
