3a8c6171fbca0f0aa7768ecaa3c07f37b901ba62
HarmGoethals <38905900+HarmGoethals@users.noreply.github.com>
2018-05-16 18:23:09 +0200

    Het werkt

--- LocatieBepaling.py
+++ LocatieBepaling.py
@@ -18,19 +18,23 @@ def channel2APDP(bestand):
     # pdp maken
     samples = []
     for n in range(locaties):
-        # elke loc overlopen
-        freq_kar = setH[:,n,:]
+        freq_kar = setH[:,n,:] # elke loc overlopen
         pdp = []
-        for i in range(freqs):
+
+        for i in range(freqs): #gemiddelde nemen van de metingen per locatie
             pdp.append(mean(freq_kar[i]))
+
+        # een venster over de frequentiekarakteristiek zetten
         hamming = np.hamming(freqs)
         filtered = hamming*pdp
         samples.append(filtered)
-    APDP = abs(ifft(samples))
+
+    APDP = abs(ifft(samples)) #inverse fourier nemen om APDP te bepalen
+
     # for x in range(locaties):
-    #     plt.figure()
     #     plt.plot(APDP[x])
-    #     plt.show()
+    # plt.show()
+
     return APDP, freqs
 
 
@@ -54,28 +58,31 @@ def APDP2delays(APDP):
         delays[i][1][0] = np.nonzero(APDP[i] == maxima_sort[index])[0]
         delays[i][1][1] = maxima_sort[index]
 
-    print delays
     return delays
 
-def calculate_location(delays, aantal_metingen):
-    
-    c = 3*10**8
+def calculate_location(delays, aantal_frequenties):
+
+
     plaatsen = np.zeros([12,2], dtype=np.double)
     locaties = len(delays)
+    precies = precies_lokatie()
+    fouten = []
     for i in range(locaties):
-        t_0 = delays[i][0][0]/(aantal_metingen*(10**7))
-        t_1 = delays[i][1][0]/(aantal_metingen*(10**7))
+        c=3*10**8
+        t_0 = delays[i][0][0]/(aantal_frequenties*(10**7))
+        t_1 = delays[i][1][0]/(aantal_frequenties*(10**7))
         afstand1 = c*t_0
         afstand2 = c*t_1
-        print(t_0)
-        print(t_1)
-        print(afstand1)
-        print(afstand2)
+
         plaatsen[i][0] = sqrt((c*t_0)**2 - (((((c*t_1)**2-(c*t_0)**2)/4)- 1)**2))
         plaatsen[i][1] = ((c*t_1)**2 - (c*t_0)**2)/4
+        fout = sqrt((plaatsen[i][0]-precies[i][0])**2 + (plaatsen[i][1]-precies[i][1])**2)
+        fouten.append(fout)
 
-    # print plaatsen
-    return plaatsen
+    gemiddelde_fout = np.mean(fouten)
+    print plaatsen
+    print "de gemiddelde fout is "+str(gemiddelde_fout)
+    return plaatsen,gemiddelde_fout
 
 def precies_lokatie():
     plaatsen = np.zeros([12,2], dtype=np.double)
@@ -83,23 +90,29 @@ def precies_lokatie():
         plaatsen[i][0] = 8+6*cos(i*pi/6)
         plaatsen[i][1] = 8+4*sin(i*pi/6)
 
-    # print(plaatsen)
     return plaatsen
-def main():
-    APDP, aantal_metingen = channel2APDP("Dataset2.mat")
 
-
-
+def final(data):
+    APDP, aantal_metingen = channel2APDP(data)
     plaatsen  = APDP2delays(APDP)
-    precies_lokatie()
-    calc_locs = calculate_location(plaatsen, aantal_metingen)
+    calc_locs,gemiddelde_fout = calculate_location(plaatsen, aantal_metingen)
     prec_locs = precies_lokatie()
+
     plt.figure()
-    for i in range (12):
+    for i in range(12):
         plt.plot(prec_locs[i][0], prec_locs[i][1], 'ro')
         plt.plot(calc_locs[i][0], calc_locs[i][1], 'go')
 
     plt.show()
+
+    with  open("resultaten_"+data[:8]+".txt","w") as out_file:
+        for i in range(len(calc_locs)):
+            out_file.write(str(calc_locs[i][0])+","+str(calc_locs[i][1])+"\n")
+        out_file.write("\nDe gemiddelde fout op de locaties is "+str(gemiddelde_fout))
+def main():
+    final("Dataset1.mat")
+    final("Dataset2.mat")
+
 
 
 if __name__ == "__main__":
